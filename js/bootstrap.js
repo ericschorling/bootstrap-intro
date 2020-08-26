1e349c3703fa046ca0c3cc47d154a9fa6c758964
Eric Schorling <eric.schorling@yahoo.com>
2020-08-26 14:07:26 -0400

    new info added

---
+++ js/bootstrap.js
@@ -0,0 +1,13 @@
+'use strict'
+
+
+$(document).ready(()=>{
+    console.log('jq is ready');
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
+    // const popoverBtn = document.querySelector('[data-toggle="popover"]');
+
+    // popoverBtn.popover();
+});
+
